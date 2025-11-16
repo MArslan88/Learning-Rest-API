ec290fd0f398ed72b67b48ef7bbb2c520fe3da40
Muhammad Arslan <gery_maz_555@hotmail.com>
2025-11-16 11:49:17 +0500

    generated get API response

--- src/main/java/com/mhdarslan/LearningRestAPIs/controller/StudentController.java
+++ src/main/java/com/mhdarslan/LearningRestAPIs/controller/StudentController.java
@@ -1,9 +1,18 @@
 package com.mhdarslan.LearningRestAPIs.controller;
 
+import com.mhdarslan.LearningRestAPIs.dto.StudentDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentController {
     @GetMapping("/student")
+    public StudentDto getStudent(){
+        return new StudentDto(4L,"Arslan", "mhdarslan@gmail.com");
+    }
+
+    @GetMapping("/student/{id}")
+    public StudentDto getStudentBy(){
+        return new StudentDto(4L,"Arslan", "mhdarslan@gmail.com");
+    }
 }
