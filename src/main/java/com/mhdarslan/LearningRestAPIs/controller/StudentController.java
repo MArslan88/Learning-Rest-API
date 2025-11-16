5de8073d0acca8d27ce1f6751c43ec55125954c2
Muhammad Arslan <gery_maz_555@hotmail.com>
2025-11-16 10:57:13 +0500

    included

---
+++ src/main/java/com/mhdarslan/LearningRestAPIs/controller/StudentController.java
@@ -0,0 +1,9 @@
+package com.mhdarslan.LearningRestAPIs.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @GetMapping("/student")
+}
